0ec72b776a64e6dbbe9c5d072dd3fc1d2a349cbe
antonio <antonio.fr.martinezc@hotmail.com>
2024-07-09 20:54:25 -0400

    added functionality to toggle mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/budget_logo.png";
 import { NavLink, useLocation } from "react-router-dom";
 import { formatCurrency } from "../utils/formatCurrency";
 function Navbar({ currentBalance }) {
 	const location = useLocation();
+	const [isMenuOpen, setIsMenuOpen] = useState();
 	function styleActive({ isActive }) {
 		return {
 			color: isActive ? "#22c55e" : "",
 		};
 	}
+	function toggleMenu() {
+		setIsMenuOpen((prev) => !prev);
+	}
+
 	function calcTextColorCurrentBal() {
 		if (currentBalance >= 2000) {
 			return "text-green-500";
@@ -28,10 +33,16 @@ function Navbar({ currentBalance }) {
 					<img src={logo} className="h-10" alt="Budget Logo" />
 				</NavLink>
 
-				<div className="hidden w-full md:block md:w-auto" id="navbar-default">
+				<div
+					className={`${
+						isMenuOpen ? "block" : "hidden"
+					} w-full md:block md:w-auto absolute top-12 left-0`}
+					id="navbar-default"
+				>
 					<ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 						<li>
 							<NavLink
+								onClick={toggleMenu}
 								style={styleActive}
 								to="/"
 								className="block py-2 px-3 text-white rounded md:p-0 "
@@ -42,6 +53,7 @@ function Navbar({ currentBalance }) {
 						</li>
 						<li>
 							<NavLink
+								onClick={toggleMenu}
 								style={styleActive}
 								to="/transactions"
 								className="block py-2 px-3 text-white rounded md:p-0 "
@@ -51,6 +63,7 @@ function Navbar({ currentBalance }) {
 						</li>
 						<li>
 							<NavLink
+								onClick={toggleMenu}
 								to="/transactions/new"
 								className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
 							>
@@ -65,11 +78,12 @@ function Navbar({ currentBalance }) {
 					</p>
 				)}
 				<button
+					onClick={toggleMenu}
 					data-collapse-toggle="navbar-default"
 					type="button"
 					className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
 					aria-controls="navbar-default"
-					aria-expanded="false"
+					aria-expanded={isMenuOpen}
 				>
 					<span className="sr-only">Open main menu</span>
 					<svg
